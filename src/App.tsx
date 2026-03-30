a33c99a6489fc8d7c7c81d2bba18b262d2ab37a6
duyhuy2005 <duyhuy2812@gmail.com>
2026-03-30 08:34:45 +0700

    thêm

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import viVN from "antd/locale/vi_VN";
 import { AuthProvider } from "./context/AuthContext";
 import { ProductProvider } from "./context/ProductContext";
 import { CartProvider } from "./context/CartContext";
+import { OrderProvider } from "./context/OrderContext";
 import MainLayout from "./components/layout/MainLayout";
 import AdminLayout from "./components/layout/AdminLayout";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
 import AboutPage from "./pages/AboutPage";
 import CustomerAuthPage from "./pages/CustomerAuthPage";
 import AdminLoginPage from "./pages/admin/AdminLoginPage";
@@ -35,34 +37,37 @@ function App() {
     >
       <AuthProvider>
         <ProductProvider>
-        <CartProvider>
-          <BrowserRouter>
-            <Routes>
-              {/* Public Routes */}
-              <Route element={<MainLayout />}>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/products" element={<ProductsPage />} />
-                <Route path="/product/:id" element={<ProductDetailPage />} />
-                <Route path="/cart" element={<CartPage />} />
-                <Route path="/about" element={<AboutPage />} />
-              </Route>
+          <CartProvider>
+            <OrderProvider>
+              <BrowserRouter>
+                <Routes>
+                  {/* Public Routes */}
+                  <Route element={<MainLayout />}>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/products" element={<ProductsPage />} />
+                    <Route path="/product/:id" element={<ProductDetailPage />} />
+                    <Route path="/cart" element={<CartPage />} />
+                    <Route path="/checkout" element={<CheckoutPage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                  </Route>
 
-              {/* Auth Pages */}
-              <Route path="/login" element={<CustomerAuthPage />} />
-              <Route path="/admin/login" element={<AdminLoginPage />} />
+                  {/* Auth Pages */}
+                  <Route path="/login" element={<CustomerAuthPage />} />
+                  <Route path="/admin/login" element={<AdminLoginPage />} />
 
-              {/* Admin Routes */}
-              <Route element={<AdminLayout />}>
-                <Route path="/admin" element={<AdminDashboard />} />
-                <Route path="/admin/products" element={<AdminProducts />} />
-                <Route path="/admin/orders" element={<AdminOrders />} />
-                <Route path="/admin/customers" element={<AdminCustomers />} />
-                <Route path="/admin/statistics" element={<AdminStatistics />} />
-                <Route path="/admin/settings" element={<AdminSettings />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </CartProvider>
+                  {/* Admin Routes */}
+                  <Route element={<AdminLayout />}>
+                    <Route path="/admin" element={<AdminDashboard />} />
+                    <Route path="/admin/products" element={<AdminProducts />} />
+                    <Route path="/admin/orders" element={<AdminOrders />} />
+                    <Route path="/admin/customers" element={<AdminCustomers />} />
+                    <Route path="/admin/statistics" element={<AdminStatistics />} />
+                    <Route path="/admin/settings" element={<AdminSettings />} />
+                  </Route>
+                </Routes>
+              </BrowserRouter>
+            </OrderProvider>
+          </CartProvider>
         </ProductProvider>
       </AuthProvider>
     </ConfigProvider>
